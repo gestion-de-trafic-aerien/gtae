89d4ee45c1b5e8cf3d93dd21a5bea961141f092b
massi <massi@massi-HP-EliteBook-820-G3>
2020-01-13 16:18:11 +0100

    zidhidzjd

--- src/dataEnum/AirportID.java
+++ src/dataEnum/AirportID.java
@@ -3,7 +3,7 @@ package dataEnum;
 import java.awt.geom.Point2D;
 
 public enum AirportID {
-	PARIS(500,600),ALGIERS(500,400),WASHINGTON(50,1000),BRAZILIA(0,50),MOSCOW(900,900),CANBERRA(1000,0);
+	PARIS(500,550),ALGIERS(500,400),WASHINGTON(50,1000),BRAZILIA(0,50),MOSCOW(900,900),CANBERRA(1000,0);
 	
 	private Point2D position;
 	
