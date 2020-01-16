fc80d1b08f7a452ec67b87b320e1060e10ba7f55
nadir <nadir@donpablo>
2020-01-16 17:09:01 +0100

    class Chrono

---
+++ src/Motor/Chrono.java
@@ -0,0 +1,51 @@
+package Motor;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Chrono extends TimerTask {
+	
+	private int hour ;
+	private int minute ;
+	
+	
+ 
+	
+	public Chrono(int hour, int minute) {
+		super();
+		this.hour = hour;
+		this.minute = minute;
+	}
+
+	
+
+
+	public int getHour() {
+		return hour;
+	}
+
+
+
+
+	public int getMinute() {
+		return minute;
+	}
+
+
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		minute ++;
+		if(minute == 60){
+			hour ++;
+			minute = 0;
+			
+		}
+		System.out.println(hour+" : "+minute);
+
+	}
+
+}
